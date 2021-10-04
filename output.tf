69892b7d3139cd47f37343fa88d7a79eabe8295b
sunilkamatec@gmail.com <ubuntu@ip-172-31-25-132.us-east-2.compute.internal>
2021-10-04 19:33:10 +0000

    first commit

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "IP-Address" {
+  value = flatten(module.container[*].IP-Address)
+  description = "IP address of the container"
+}
+
+output "Container-Name" {
+  value = module.container[*].Container-Name
+  description = "Name of the container"
+}
---
+++ variables.tf
@@ -0,0 +1,42 @@
+# variable "env" {
+#   type = string
+#   description = "Env to deploy to"
+#   default = "dev"
+# }
+
+variable "image" {
+  type = map
+  description = "Image for container"
+  default = {
+    dev = "nodered/node-red:latest"
+    prod = "nodered/node-red:latest-minimal"
+  }
+}
+
+variable "ext_port" {
+  type = map
+  
+  validation {
+    condition = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) > 0
+    error_message = "The ext port should be between 65535 and Zero."
+  }
+  validation {
+    condition = max(var.ext_port["prod"]...) <= 65535 && min(var.ext_port["prod"]...) > 0
+    error_message = "The ext port should be between 65535 and Zero."
+  }
+}
+
+variable "int_port" {
+  type = number
+  default = 1880
+  
+  validation {
+    condition = var.int_port == 1880
+    error_message = "The internal port must be 8080."
+  }
+}
+
+
+locals {
+  container_count = length(var.ext_port[terraform.workspace])
+}
